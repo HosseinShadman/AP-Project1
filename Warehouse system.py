41956e57c739dc9b3246d76ea32bddb17c78f53b
HosseinShadman <128729049+HosseinShadman@users.noreply.github.com>
2023-04-23 14:00:20 +0330

    Defining manager update stock functions

--- Warehouse system.py
+++ Warehouse system.py
@@ -18,6 +18,22 @@ class Warehouse:  # a class for warehouse information
             if product.code == code:
                 product.stock = product.stock - buyed_quantity
                 break
+    
+    def manager_update_stock_from_textfile(self, file_path):
+        with open(file_path, 'r') as f:  # reading a txt file
+            for line in f:
+                code, new_stock = line.strip().split(',')
+                self.update_stock(code, int(new_stock))
+    
+    def manager_update_stock_from_csvfile(self, file_path):
+        d = pd.read_csv(file_path)  # reading a txt file with pandas library
+        for i in d.index:
+            self.update_stock(d['code'][i], d['new_stock'][i])
+            
+    def manager_update_stock_manually(self):
+        code = input("Enter product code: ")
+        new_stock = int(input("Enter new stock: "))
+        self.update_stock(code, new_stock)
 
     def check_availability(self, code, quantity):
         for product in self.products:
