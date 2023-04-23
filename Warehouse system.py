4086d3a43322460b15ef86f3ee07af2e7285db6b
HosseinShadman <128729049+HosseinShadman@users.noreply.github.com>
2023-04-23 13:57:47 +0330

    Creating Warehouse class

--- Warehouse system.py
+++ Warehouse system.py
@@ -5,3 +5,24 @@ class Product:  # main class for product information
         self.code = code
         self.name = name
         self.stock = stock
+        
+class Warehouse:  # a class for warehouse information
+    def __init__(self):
+        self.products = []  # The warehouse list
+
+    def add_product(self, product):
+        self.products.append(product)
+
+    def update_stock(self, code, buyed_quantity):
+        for product in self.products:
+            if product.code == code:
+                product.stock = product.stock - buyed_quantity
+                break
+
+    def check_availability(self, code, quantity):
+        for product in self.products:
+            if product.code == code:
+                if product.stock >= quantity:
+                    return True
+                else:
+                    return False
