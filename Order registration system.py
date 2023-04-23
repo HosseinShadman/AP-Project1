2948c1dd5dfcb0f5e93dde701f44a80bfc608906
HosseinShadman <128729049+HosseinShadman@users.noreply.github.com>
2023-04-23 13:36:54 +0330

    Start coding

---
+++ Order registration system.py
@@ -0,0 +1,7 @@
+import random  # importing random library
+
+class Product:  # main class for product information
+    def __init__(self, name, price, attributes):
+        self.name = name
+        self.price = price
+        self.attributes = attributes
