c39d0f9c138caff636df69cb17d6b0e39fe0b885
HosseinShadman <128729049+HosseinShadman@users.noreply.github.com>
2023-04-23 13:39:46 +0330

    Creating Cart class and its attributes

--- Order registration system.py
+++ Order registration system.py
@@ -5,3 +5,19 @@ class Product:  # main class for product information
         self.name = name
         self.price = price
         self.attributes = attributes
+
+class Cart:  # a class for cart information
+    def __init__(self):
+        self.items = []  # The cart list
+
+    def add_item(self, product, quantity, attributes):
+        self.items.append({'product': product, 'quantity': quantity, 'attributes': attributes})
+
+    def remove_item(self, item):
+        self.items.remove(item)
+
+    def get_total_price(self):
+        total_price = 0
+        for item in self.items:
+            total_price += item['product'].price * item['quantity']
+        return total_price
