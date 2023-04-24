435ac2f9eb0fcfc7087a9a2b182b7c73c9b5baa8
GoddamnSamE <129541597+GoddamnSamE@users.noreply.github.com>
2023-04-24 22:20:26 +0330

    defining the main class

--- Accounting_system.py
+++ Accounting_system.py
@@ -1,3 +1,12 @@
 import pandas as pd # importing pandas library
 
 from Order_registration_system import Order # useing Order_registration_system module
+
+class Successful_orders:  # main class for successful orders
+    def __init__(self):
+        self.orders = []  # The orders list
+        
+    def add_orders(self, number_of_goods, net_order_price, shipping_cost):
+        order_number = Order().order_number
+        tax = net_order_price * 0.09
+        self.orders.append([number_of_goods, order_number, net_order_price, shipping_cost, tax])
