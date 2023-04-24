fb1eae29a5128c836fb9545ae3efb897fadbec7a
GoddamnSamE <129541597+GoddamnSamE@users.noreply.github.com>
2023-04-24 22:37:47 +0330

    defining choose_delivery_time function

--- Logistic_system.py
+++ Logistic_system.py
@@ -4,6 +4,8 @@ class Address:  # main class
         self.city_code = city_code
         self.details = details
         self.postal_code = postal_code
+        self.afternoon_delivery_capacity = 3 # basic capacity for afternoon
+        self.evening_delivery_capacity = 3   # basic capacity for evening
 
     def check_address(self):  # check correct address features
         # country code should be 1 for Tehran OR 2 for Esfahan OR 3 for Tabriz...
@@ -19,3 +21,17 @@ class Address:  # main class
             return 'courier'
         else:
             return 'post'
+            
+    def choose_delivery_time(self):
+        available_delivery_times = []  
+        available_delivery_times.append('morning')  
+        if self.afternoon_delivery_capacity > 0:  
+            available_delivery_times.append('afternoon')
+        if self.evening_delivery_capacity > 0:
+            available_delivery_times.append('evening')  
+        delivery_time = input(f'Enter delivery time {available_delivery_times}: ')
+        if delivery_time == 'evening':
+            self.evening_delivery_capacity -= 1  
+        if delivery_time == 'afternoon':
+            self.afternoon_delivery_capacity -= 1  
+        return delivery_time
