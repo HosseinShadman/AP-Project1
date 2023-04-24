c98c76bcb218a52dbea101ce124e7c1e7c933058
GoddamnSamE <129541597+GoddamnSamE@users.noreply.github.com>
2023-04-24 22:33:19 +0330

    defining chack address function

--- Logistic_system.py
+++ Logistic_system.py
@@ -4,3 +4,12 @@ class Address:  # main class
         self.city_code = city_code
         self.details = details
         self.postal_code = postal_code
+
+    def check_address(self):  # check correct address features
+        # country code should be 1 for Tehran OR 2 for Esfahan OR 3 for Tabriz...
+        # city code should be between the numbers 1 OR 2
+        # postal code should have 10 digits
+        if self.country_code in range(1,4) and self.city_code in range(1,3) and len(self.postal_code) == 10:
+            return 'address is correct'
+        else:
+            return 'address is incorrect'
